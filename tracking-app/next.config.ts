b044aa8fe1447502eca7bd5d7782592e6fb2ed73
hugoc1944 <125464948+hugoc1944@users.noreply.github.com>
2025-09-04 03:23:58 +0100

    Fix on turbopack

--- tracking-app/next.config.ts
+++ tracking-app/next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   // (optional) silence the multi-lockfile warning by pinning the root
-  turbopack: { root: './tracking-app' },
 };
 
 
